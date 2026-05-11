ebe03840e2bf94a1b4aa45ba0bb1d10a65d9e22d
Scott R McGann <s.r.mcgann@hotmail.com>
2026-05-11 13:41:39 -0700

    Add files via upload

--- coordinates.js
+++ coordinates.js
@@ -6989,72 +6989,81 @@ const ApplyLocation = shape => {
 }
 
 const ApplyRotation = shape => {
-  var ax = 0, ay = 0, az = 0, ct = 0, x, y, z, p, d
-  var cx = shape.x
-  var cy = shape.y
-  var cz = shape.z
-  for(var i = 0; i < shape.vertices.length; i+=3){
-    x = shape.vertices[i+0]
-    y = shape.vertices[i+1]
-    z = shape.vertices[i+2]
-    switch(shape.rotationMode){
-      case 0:
-        p = Math.atan2(x, y)
-        d = Math.hypot(x, y)
-        x = S(p) * d
-        y = C(p) * d
-        p = Math.atan2(x, z)
-        d = Math.hypot(x, z)
-        x = S(p) * d
-        z = C(p) * d
-        p = Math.atan2(y, z)
-        d = Math.hypot(y, z)
-        y = S(p) * d
-        z = C(p) * d
-      case 1:
-        p = Math.atan2(y, z)
-        d = Math.hypot(y, z)
-        y = S(p) * d
-        z = C(p) * d
-        p = Math.atan2(x, z)
-        d = Math.hypot(x, z)
-        x = S(p) * d
-        z = C(p) * d
-        p = Math.atan2(x, y)
-        d = Math.hypot(x, y)
-        x = S(p) * d
-        y = C(p) * d
-      case 2:
-        p = Math.atan2(x, z)
-        d = Math.hypot(x, z)
-        x = S(p) * d
-        z = C(p) * d
-        p = Math.atan2(y, z)
-        d = Math.hypot(y, z)
-        y = S(p) * d
-        z = C(p) * d
-        p = Math.atan2(x, y)
-        d = Math.hypot(x, y)
-        x = S(p) * d
-        y = C(p) * d
-      case 3:
-        p = Math.atan2(x, z)
-        d = Math.hypot(x, z)
-        x = S(p) * d
-        z = C(p) * d
-        p = Math.atan2(y, z)
-        d = Math.hypot(y, z)
-        y = S(p) * d
-        z = C(p) * d
-        p = Math.atan2(x, y)
-        d = Math.hypot(x, y)
-        x = S(p) * d
-        y = C(p) * d
+  var x, y, z, p, d, component
+  for(var m = 3; m--;){
+    switch(m){
+      case 0: component = 'vertices'; break
+      case 1: component = 'normalVecs'; break
+      case 2: component = 'normals'; break
     }
-    shape.vertices[i+0] = x
-    shape.vertices[i+1] = y
-    shape.vertices[i+2] = z
+    for(var i = 0; i < shape[component].length; i+=3){
+      x = shape[component][i+0]
+      y = shape[component][i+1]
+      z = shape[component][i+2]
+      switch(shape.rotationMode){
+        case 0:
+          p = Math.atan2(x, y) + shape.roll
+          d = Math.hypot(x, y)
+          x = S(p) * d
+          y = C(p) * d
+          p = Math.atan2(x, z) + shape.yaw
+          d = Math.hypot(x, z)
+          x = S(p) * d
+          z = C(p) * d
+          p = Math.atan2(y, z) + shape.pitch
+          d = Math.hypot(y, z)
+          y = S(p) * d
+          z = C(p) * d
+        break
+        case 1:
+          p = Math.atan2(y, z) + shape.pitch
+          d = Math.hypot(y, z)
+          y = S(p) * d
+          z = C(p) * d
+          p = Math.atan2(x, z) + shape.yaw
+          d = Math.hypot(x, z)
+          x = S(p) * d
+          z = C(p) * d
+          p = Math.atan2(x, y) + shape.roll
+          d = Math.hypot(x, y)
+          x = S(p) * d
+          y = C(p) * d
+        break
+        case 2:
+          p = Math.atan2(x, z) + shape.yaw
+          d = Math.hypot(x, z)
+          x = S(p) * d
+          z = C(p) * d
+          p = Math.atan2(y, z) + shape.pitch
+          d = Math.hypot(y, z)
+          y = S(p) * d
+          z = C(p) * d
+          p = Math.atan2(x, y) + shape.roll
+          d = Math.hypot(x, y)
+          x = S(p) * d
+          y = C(p) * d
+        break
+        case 3:
+          p = Math.atan2(x, z) + shape.yaw
+          d = Math.hypot(x, z)
+          x = S(p) * d
+          z = C(p) * d
+          p = Math.atan2(y, z) + shape.pitch
+          d = Math.hypot(y, z)
+          y = S(p) * d
+          z = C(p) * d
+          p = Math.atan2(x, y) + shape.roll
+          d = Math.hypot(x, y)
+          x = S(p) * d
+          y = C(p) * d
+        break
+      }
+      shape[component][i+0] = x
+      shape[component][i+1] = y
+      shape[component][i+2] = z
+    }
   }
+  shape.yaw = shape.pitch = shape.roll = 0
 }
 
 const ApplyScale = shape => {
